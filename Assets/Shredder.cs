6c19c27fca2c13b8bc179b05d9613006dd0989b0
rectacrab <hanleyben@gmail.com>
2018-08-01 07:36:10 +0100

    fkn whatevs
    fk mate

---
+++ Assets/Shredder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //destroy projecticles that touch you.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Projectile>())
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
